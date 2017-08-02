032c8a2a03f01cd1a3e5cff6f847bb31c65d6b1d
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2017-08-02 13:57:54 -0500

    adding distinct tables

--- bens_code/copy_from_pi/mentions_distinct_table.sql
+++ bens_code/copy_from_pi/mentions_distinct_table.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS mentions_distinct;
 CREATE TABLE mentions_distinct AS
 SELECT DISTINCT created_at
-,tweet_id string
+,tweet_id
 ,mentioned_user_id
 ,mentioned_user_name
 ,mentioned_user_screen_name
