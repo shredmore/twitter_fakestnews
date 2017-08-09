4419dd1ec3d0cc8492f2ca002c5b80974c040f64
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2017-08-08 20:38:08 -0500

    adding queries

--- bens_code/queries/users_count_by_day.sql
+++ bens_code/queries/users_count_by_day.sql
@@ -8,6 +8,6 @@ JOIN
 ,username
 ,MAX(followers_count) followers
 FROM users_distinct
-GROUP BY user_id, username) users_distinct b
+GROUP BY user_id, username) b
 ON a.user_id = b.user_id
 GROUP BY CONCAT(SUBSTRING(a.created_at,5,7),SUBSTRING(a.created_at,27,4)), a.username;
