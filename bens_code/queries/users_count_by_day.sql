3b0b73d2d8dcabb153c84e9e20675d7df2e402f8
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2017-08-08 20:37:07 -0500

    adding queries

--- bens_code/queries/users_count_by_day.sql
+++ bens_code/queries/users_count_by_day.sql
@@ -1,8 +1,13 @@
-SELECT CONCAT(SUBSTRING(created_at,5,7),SUBSTRING(created_at,27,4)) year_month_day
-,username
-,MAX(followers_count) followers
+SELECT CONCAT(SUBSTRING(a.created_at,5,7),SUBSTRING(a.created_at,27,4)) year_month_day
+,a.username
+,b.followers
 ,COUNT(*) counts
 FROM tweets_2_distinct a
-JOIN users_distinct b
+JOIN 
+(SELECT user_id
+,username
+,MAX(followers_count) followers
+FROM users_distinct
+GROUP BY user_id, username) users_distinct b
 ON a.user_id = b.user_id
-GROUP BY CONCAT(SUBSTRING(created_at,5,7),SUBSTRING(created_at,27,4)), username;
+GROUP BY CONCAT(SUBSTRING(a.created_at,5,7),SUBSTRING(a.created_at,27,4)), a.username;
