e1b95672ce630043576051a2f1c6b64bec26634c
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2017-08-08 20:15:59 -0500

    adding queries

--- bens_code/queries/users_count_by_day.sql
+++ bens_code/queries/users_count_by_day.sql
@@ -1,3 +1,3 @@
 SELECT CONCAT(SUBSTRING(created_at,5,7),SUBSTRING(created_at,27,4)) year_month_day, username, COUNT(*) counts
-FROM hashtags_distinct
+FROM tweets_2_distinct
 GROUP BY CONCAT(SUBSTRING(created_at,5,7),SUBSTRING(created_at,27,4)), username;
