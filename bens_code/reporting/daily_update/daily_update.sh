9e4df38700785801c5bf6f764f3cf34c52bd7b4b
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2017-08-15 21:20:58 -0500

    updating reporting

--- bens_code/reporting/daily_update/daily_update.sh
+++ bens_code/reporting/daily_update/daily_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 (echo "yesterday's headlines collected by news site"\
 	;hive -f yesterday_headline_count.sql\
-	;echo "number of tweets collected yesterday"\
+	;echo "number of tweets by day"\
 	;hive -f yesterday_tweet_count.sql\
 	;echo "top #fakenews users and times used"\
 	;hive -f top_fakenews_users.sql) | mail -s "report1" ben.thompson.j@gmail.com
