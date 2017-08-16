e2a7fe7fb2dc200f69470471fb94781b4d6a23e1
Ben Thompson <benthompson@Bens-MacBook-Pro.local>
2017-08-15 20:31:55 -0500

    updating reporting

--- bens_code/reporting/daily_update/yesterday_headline_count.sql
+++ bens_code/reporting/daily_update/yesterday_headline_count.sql
@@ -2,6 +2,6 @@
 SELECT to_date(date_time) the_date
 ,site_code
 ,COUNT(*)
-FROM headlines
-WHERE to_date(date_time) = date_sub(current_date(),1)
+FROM headlines a
+WHERE to_date(a.date_time) IN (SELECT MAX(to_date(date_time)) FROM headlines)
 GROUP BY to_date(date_time), site_code;
